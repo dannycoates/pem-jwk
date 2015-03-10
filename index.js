1fe1d3f101640cce88a412313465f03438fb109a
Danny Coates <dannycoates@gmail.com>
2015-03-10 14:02:11 -0700

    live long and prosper

---
+++ index.js
@@ -0,0 +1,172 @@
+var asn = require('asn1.js')
+var base64url = require('base64url')
+
+function fromPEM(data) {
+  var text = data.toString().split(/(\r\n|\r|\n)+/g);
+  text = text.filter(function(line) {
+    return line.trim().length !== 0;
+  });
+  text = text.slice(1, -1).join('');
+  return new Buffer(text.replace(/[^\w\d\+\/=]+/g, ''), 'base64');
+}
+
+var RSAPublicKey = asn.define('RSAPublicKey', function () {
+  this.seq().obj(
+    this.key('n').int(),
+    this.key('e').int()
+  )
+})
+
+var AlgorithmIdentifier = asn.define('AlgorithmIdentifier', function () {
+  this.seq().obj(
+    this.key('algorithm').objid(),
+    this.key('parameters').optional().any()
+  )
+})
+
+var PublicKeyInfo = asn.define('PublicKeyInfo', function () {
+  this.seq().obj(
+    this.key('algorithm').use(AlgorithmIdentifier),
+    this.key('publicKey').bitstr()
+  )
+})
+
+var Version = asn.define('Version', function () {
+  this.int({
+    0: 'two-prime',
+    1: 'multi'
+  })
+})
+
+var OtherPrimeInfos = asn.define('OtherPrimeInfos', function () {
+  this.seq().obj(
+    this.key('ri').int(),
+    this.key('di').int(),
+    this.key('ti').int()
+  )
+})
+
+var RSAPrivateKey = asn.define('RSAPrivateKey', function () {
+  this.seq().obj(
+    this.key('version').use(Version),
+    this.key('n').int(),
+    this.key('e').int(),
+    this.key('d').int(),
+    this.key('p').int(),
+    this.key('q').int(),
+    this.key('dp').int(),
+    this.key('dq').int(),
+    this.key('qi').int(),
+    this.key('other').optional().use(OtherPrimeInfos)
+  )
+})
+
+var PrivateKeyInfo = asn.define('PrivateKeyInfo', function () {
+  this.seq().obj(
+    this.key('version').use(Version),
+    this.key('algorithm').use(AlgorithmIdentifier),
+    this.key('privateKey').bitstr()
+  )
+})
+
+const RSA_OID = '1.2.840.113549.1.1.1'
+
+function decodeRsaPublic(buffer) {
+  var key = RSAPublicKey.decode(buffer, 'der')
+  var e = key.e.toString(16)
+  if (e.length % 2 === 1) { e = '0' + e }
+  return {
+    kty: 'RSA',
+    n: bn2base64url(key.n),
+    e: base64url(e, 'hex')
+  }
+}
+
+function decodeRsaPrivate(buffer) {
+  var key = RSAPrivateKey.decode(buffer, 'der')
+  var e = key.e.toString(16)
+  if (e.length % 2 === 1) { e = '0' + e }
+  return {
+    kty: 'RSA',
+    n: bn2base64url(key.n),
+    e: base64url(e, 'hex'),
+    d: bn2base64url(key.d),
+    p: bn2base64url(key.p),
+    q: bn2base64url(key.q),
+    dp: bn2base64url(key.dp),
+    dq: bn2base64url(key.dq),
+    qi: bn2base64url(key.qi)
+  }
+}
+
+function decodePublic(buffer) {
+  var info = PublicKeyInfo.decode(buffer, 'der')
+  return decodeRsaPublic(info.publicKey.data, 'der')
+}
+
+function decodePrivate(buffer) {
+  var info = PrivateKeyInfo.decode(buffer, 'der')
+  return decodeRsaPrivate(info.privateKey.data, 'der')
+}
+
+function getDecoder(header) {
+  var match = /^-----BEGIN (RSA )?(PUBLIC|PRIVATE) KEY-----$/.exec(header)
+  if (!match) { return null }
+  var isRSA = !!(match[1])
+  var isPrivate = (match[2] === 'PRIVATE')
+  if (isPrivate) {
+    return isRSA ? decodeRsaPrivate : decodePrivate
+  }
+  else {
+    return isRSA ? decodeRsaPublic : decodePublic
+  }
+}
+
+function pem2jwk(pem) {
+  var text = pem.toString().split(/(\r\n|\r|\n)+/g)
+  text = text.filter(function(line) {
+    return line.trim().length !== 0
+  });
+  var decoder = getDecoder(text[0])
+
+  text = text.slice(1, -1).join('')
+  return decoder(new Buffer(text.replace(/[^\w\d\+\/=]+/g, ''), 'base64'))
+}
+
+function jwk2pem(jwk) {
+  var isPrivate = !!(jwk.d)
+  var t = isPrivate ? 'PRIVATE' : 'PUBLIC'
+  var header = '-----BEGIN RSA ' + t + ' KEY-----\n'
+  var footer = '\n-----END RSA ' + t + ' KEY-----\n'
+  var data = isPrivate ?
+    RSAPrivateKey.encode({
+      version: 'two-prime',
+      n: base64url2bn(jwk.n),
+      e: base64url2bn(jwk.e),
+      d: base64url2bn(jwk.d),
+      p: base64url2bn(jwk.p),
+      q: base64url2bn(jwk.q),
+      dp: base64url2bn(jwk.dp),
+      dq: base64url2bn(jwk.dq),
+      qi: base64url2bn(jwk.qi)
+    }, 'der') :
+    RSAPublicKey.encode({
+      n: base64url2bn(jwk.n),
+      e: base64url2bn(jwk.e)
+    }, 'der')
+  var body = data.toString('base64').match(/.{1,64}/g).join('\n')
+  return header + body + footer
+}
+
+function bn2base64url(bn) {
+  return base64url(bn.toString(16), 'hex')
+}
+
+function base64url2bn(str) {
+  return new asn.bignum(base64url.toBuffer(str))
+}
+
+module.exports = {
+  pem2jwk: pem2jwk,
+  jwk2pem: jwk2pem
+}
